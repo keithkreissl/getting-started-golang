3af9dc05fc2ee7f2e0536b5cc79ea8456afe0a80
Keith Kreissl <keithkreissl@gmail.com>
2015-08-11 10:19:15 -0500

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CityHandler(res http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York, Tokyo, Chicago, Wheaton, blah, Glen Ellyn'}")
+	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York, Tokyo, Chicago, Wheaton, blah, Glen Ellyn, Carol Stream'}")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
 }
